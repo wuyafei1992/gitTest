cffb1f026b3f4c7fca033fc84e3fd895e72a4027
wuyafei <904301006@qq.com>
2021-12-11 18:58:26 +0800

    edit

--- src/main/java/com/wuyafei/TestGit.java
+++ src/main/java/com/wuyafei/TestGit.java
@@ -7,7 +7,7 @@ package com.wuyafei;
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git-edit");
         System.out.println("TestGit");
     }
 }
