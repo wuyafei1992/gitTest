8d96af5ba5db6eae4ea9f3bcbfedb910b0a48a07
wuyafei <904301006@qq.com>
2021-12-11 18:11:22 +0800

    first commit

---
+++ src/main/java/com/wuyafei/TestGit.java
@@ -0,0 +1,12 @@
+package com.wuyafei;
+
+/**
+ * @author wuyafei
+ * @version 1.0
+ * @date 2021/12/11 18:00
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("TestGit");
+    }
+}
